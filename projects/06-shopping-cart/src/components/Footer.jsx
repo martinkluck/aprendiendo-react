9154022bc10024feab28ea199bc21aac94be5bb7
Martin Gustavo Kluck <martinkluck05@gmail.com>
2023-05-06 19:47:04 -0300

    Finish 06-shopping-cart

--- projects/06-shopping-cart/src/components/Footer.jsx
+++ projects/06-shopping-cart/src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import './Footer.css';
+// import { useCart } from '../hooks/useCart';
 
 export function Footer() {
   // const { filters } = useFilters()
+  // const { cart } = useCart();
 
   return (
     <footer className='footer'>
       <h4>
-        Prueba técnica de React ⚛️ － <span>@midudev</span>
+        Prueba técnica de React ⚛️ － <span>@mgkluck</span>
       </h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
