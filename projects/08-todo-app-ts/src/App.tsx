00bcba13dc3a57715099d6e7d064e565db451f11
Martin Gustavo Kluck <martinkluck05@gmail.com>
2023-07-09 19:46:37 -0300

    Ficnish class 08

--- projects/08-todo-app-ts/src/App.tsx
+++ projects/08-todo-app-ts/src/App.tsx
@@ -1,28 +1,102 @@
-import { useState } from "react";
-import { Todos } from "./components/Todos";
+import { useState } from 'react';
+import { Todos } from './components/Todos';
+import { type Todo as TodoType, type TodoId, type FilterValue, type TodoTitle } from './types';
+import { TODO_FILTERS } from './constants';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 
 const mockTodos = [
   {
-    id: 1,
+    id: '1',
     title: 'Buy milk',
-    completed: false,
+    completed: true,
   },
   {
-    id: 2,
+    id: '2',
     title: 'Buy eggs',
     completed: false,
   },
   {
-    id: 3,
+    id: '3',
     title: 'Buy bread',
     completed: false,
   },
 ];
 
 const App = (): JSX.Element => {
-  const [todos] = useState(mockTodos);
+  const [todos, setTodos] = useState(mockTodos);
+  const [filterSelected, setFilterSelected] = useState<FilterValue>(TODO_FILTERS.ALL);
+
+  const handleRemove = ({ id }: TodoId): void => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  const handleCompleted = ({
+    id,
+    completed,
+  }: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
+  const handleFilterChange = (filter: FilterValue): void => {
+    setFilterSelected(filter);
+  };
+
+  const handleClearCompleted = (): void => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTodos(newTodos);
+  };
+
+  const handleAddTodo = ({title}: TodoTitle): void => {
+    const newTodo: TodoType = {
+      id: crypto.randomUUID(),
+      title,
+      completed: false,
+    };
+    setTodos([newTodo, ...todos]);
+  };
+
+  const activeCount = todos.filter((todo) => !todo.completed).length;
+  const completedCount = todos.length - activeCount;
+
+  const filteredTodos = todos.filter((todo) => {
+    switch (filterSelected) {
+      case TODO_FILTERS.ACTIVE:
+        return !todo.completed;
+      case TODO_FILTERS.COMPLETED:
+        return todo.completed;
+      default:
+        return true;
+    }
+  }
+  );
+
   return (
-    <Todos todos={todos} />
+    <div className='todoapp'>
+      <Header onAddTodo={handleAddTodo} />
+      <Todos
+        onCompleted={handleCompleted}
+        onRemoveTodo={handleRemove}
+        todos={filteredTodos}
+      />
+      <Footer
+        activeCount={activeCount}
+        completedCount={completedCount}
+        filterSelected={filterSelected}
+        onClearCompleted={handleClearCompleted}
+        handleFilterChange={handleFilterChange}
+      />
+    </div>
   );
 };
 
--- projects/08-todo-app-ts/src/components/Todos.tsx
+++ projects/08-todo-app-ts/src/components/Todos.tsx
@@ -1,14 +1,29 @@
-import { type ListOfTodos } from "../types";
+import { type TodoId, type ListOfTodos, type Todo as TodoType } from '../types';
+import { Todo } from './Todo';
 
 interface Props {
   todos: ListOfTodos;
+  onRemoveTodo: ({ id }: TodoId) => void;
+  onCompleted: ({ id, completed }: Pick<TodoType, 'id' | 'completed'>) => void;
 }
 
-export const Todos: React.FC<Props> = ({ todos }) => {
+export const Todos: React.FC<Props> = ({ todos, onRemoveTodo, onCompleted }) => {
   return (
-    <ul>
+    <ul className='todo-list'>
       {todos.map((todo) => (
-        <li key={todo.id}>{todo.title}</li>
+        <li
+          key={todo.id}
+          className={`${todo.completed ? 'completed' : ''}`}
+        >
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+            onRemoveTodo={onRemoveTodo}
+            onCompleted={onCompleted}
+          />
+        </li>
       ))}
     </ul>
   );
