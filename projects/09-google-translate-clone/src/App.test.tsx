ab68d0927595b4f537c81af8c0b0b5b6ff9df041
Martin Gustavo Kluck <martinkluck05@gmail.com>
2023-07-22 20:31:08 -0300

    Fix test

--- projects/09-google-translate-clone/src/App.test.tsx
+++ projects/09-google-translate-clone/src/App.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it } from 'vitest'
+import { test, expect } from 'vitest'
 import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
@@ -6,7 +6,7 @@ import App from './App'
 test('My App works as expected', async () => {
   const user = userEvent.setup()
   const app = render(<App />)
-  const textareaFrom = app.getByPlaceHolderText('Introducir texto')
+  const textareaFrom = app.getByPlaceholderText('Introducir texto')
 
   await user.type(textareaFrom, 'Hola Mundo')
   const result = await app.findByDisplayValue(/Hello world/i, {}, { timeout: 2000 })
